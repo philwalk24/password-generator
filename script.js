365500e4e088a98e501a44dd9a827f8972da74a3
Phillip Walker <phillipw200024@gmail.com>
2023-04-20 22:20:24 -0400

    added a comment

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ function getRandInt(maximum)
 // for each type of character
 function generatePassword(l,u,lo,n,s)
 {
+  // someone posted these in the #resources channel
   var nums = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?", "~"];
 var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
